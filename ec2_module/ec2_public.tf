d946f97783290c1a6456f81f76241b0a88e52da1
radwanmaazon <redwan30503321@gmail.com>
2024-07-27 16:00:30 +0300

    Module files

--- ec2_module/ec2_public.tf
+++ ec2_module/ec2_public.tf
@@ -1,13 +1,13 @@
-# resource "aws_instance" "public_instance" {
-#     ami               = data.aws_ami.latest_amazon_linux.id
-#     instance_type     = var.instance_type
-#     subnet_id         = var.public_subnet_id
-#     key_name          = var.key_pair_name
-#     security_groups   = [aws_security_group.allow_tls_SG.id]
-#     iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-#     tags = {
-#         Name = "${var.project_name}-public_ec2"
-#     }
-# }
+resource "aws_instance" "public_instance" {
+    ami               = data.aws_ami.latest_amazon_linux.id
+    instance_type     = var.instance_type
+    subnet_id         = var.public_subnet_id
+    key_name          = var.key_pair_name
+    security_groups   = [aws_security_group.allow_tls_SG.id]
+    iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+    tags = {
+        Name = "${var.project_name}-public_ec2"
+    }
+}
 
 
